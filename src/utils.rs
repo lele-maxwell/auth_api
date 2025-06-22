29884950f215e8c00f1c4fd5a23e326f85b5dc16
lele-maxwell <leleivanlele22@gmail.com>
2025-06-22 18:40:55 +0100

    fix(env): correct JWT expiration variable name and update value to seconds

--- src/utils.rs
+++ src/utils.rs
@@ -4,11 +4,11 @@ pub struct Config {
     pub jwt_duration_sec: u32,
 }
 
-pub fn load_env() -> Config {
+pub fn load_env() -> Config { 
   dotenv::dotenv().ok();
 
   let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
-  let jwt_duration_sec = std::env::var("JWT_DURATION").expect("JWT_DURATION must be set");
+  let jwt_duration_sec = std::env::var("JWT_EXPIRATION").expect("JWT_DURATION must be set");
   let jwt_duration_sec = jwt_duration_sec.parse::<u32>().expect("JWT_DURATION must be a number");
 
   return Config {
